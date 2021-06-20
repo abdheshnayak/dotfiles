37ea4364ff3a4a2f61511c1973fdb56022b4fb65
abdheshnayak <abdheshnayak@gmail.com>
2021-06-20 11:54:55 +0545

    :sparkles: installer added

---
+++ install_config.sh
@@ -0,0 +1,24 @@
+sudo pacman -S feh bspwm sxhkd maim xclip picom ttf-font-awesome rofi base-devel git
+
+
+git clone git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+sudo rm -r yay
+
+git clone https://aur.archlinux.org/xst-git.git
+cd xst-git
+makepkg -si
+cd ..
+sudo rm -r xst-git
+
+git clone https://aur.archlinux.org/st-git.git
+cd st-git
+makepkg -si
+cd ..
+sudo rm -r st-git
+
+sudo rm -r .git
+
+cp -r . ~/

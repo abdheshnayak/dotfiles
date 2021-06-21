d077344508d64ee84b30409954e511463456726a
abdheshnayak <abdheshnayak@gmail.com>
2021-06-21 10:22:33 +0545

    :bug: .

--- install_arch.sh
+++ install_arch.sh
@@ -1,14 +1,14 @@
-cd &
+cd 
 
-pacstrap -i /mnt base linux linux-firmware sudo nano &
+pacstrap -i /mnt base linux linux-firmware sudo nano 
 
-genfstab -U -p /mnt >> /mnt/etc/fstab &
+genfstab -U -p /mnt >> /mnt/etc/fstab 
 
-arch-chroot /mnnt /bin/bash &
+arch-chroot /mnnt /bin/bash 
 
-cd &
+cd 
 
-pacman -S grub networkmanager gdm xfce4-terminal git &
-systemctl enable gdm &
+pacman -S grub networkmanager gdm xfce4-terminal git 
+systemctl enable gdm 
 systemctl enable NetworkManager
 

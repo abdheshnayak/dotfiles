08d2088029fb69272c1828045b8a1a502891befb
abdheshnayak <abdheshnayak@gmail.com>
2021-06-21 09:34:50 +0545

    :bug: .

---
+++ install_arch.sh
@@ -0,0 +1,10 @@
+cd
+pacstrap -i /mnt base linux linux-firmware sudo nano
+
+genfstab -U -p /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnnt /bin/bash
+
+pacman -S grub networkmanager gdm xfce4-terminal git 
+systemctl enable gdm
+systemctl enable NetworkManager

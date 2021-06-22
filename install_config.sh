532ec4b6e0c9952b20d504e988aaea1d251c3e71
abdheshnayak <abdheshnayak@gmail.com>
2021-06-22 09:17:27 +0545

    :construction: .

--- install_config.sh
+++ install_config.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
+
 # functions
 
 show_intro(){
     cat << EOF
             ############################################
-            #    Script processes the following        #
+            #           Installing packages            #
             ############################################
-            #            1. Install Packages           #
-            #            2. i3 setup                   #
-            #            3. bspwm setup                #
-            #            4. zsh Setup                  # 
-            #            5. rofi Setup                 #
-            #            6. ranger Setup               #
-            ############################################
+
+            PACKAGES: bspwm sxhkd feh i3-gaps maim xclip
+                    picom ttf-font-awesome rofi 
+                    base-devel git base-devel zsh dmenu
+                    ranger htop
+
+            #############################################
 EOF
+}
+
+install_packages(){
+    sudo pacman -S bspwm sxhkd feh i3-gaps maim xclip picom ttf-font-awesome rofi base-devel git base-devel zsh dmenu ranger htop
+}
+
+create_work_dir(){
+    ([[ -d $dir ]] || [[ -L $dir ]]) && \
+        sudo rm -r $dir
 }
 
 setup_lf(){
@@ -39,79 +49,85 @@ EOF
     cd ~/abdhesh_lkjd/yay
     makepkg -si
 }
-# Tasks to be done
 
-
-cat << EOF
+setup_xfsudo(){
+    cat << EOF
             ############################################
-            #           Installing packages            #
+                Installing xfsudo password prompt    
             ############################################
-
-            PACKAGES: bspwm sxhkd feh i3-gaps maim xclip
-                    picom ttf-font-awesome rofi 
-                    base-devel git base-devel zsh dmenu
-                    ranger htop
-
-            #############################################
 EOF
 
-sudo pacman -S bspwm sxhkd feh i3-gaps maim xclip picom ttf-font-awesome rofi base-devel git base-devel zsh dmenu ranger htop
+    git clone https://aur.archlinux.org/xfsudo.git ~/abdhesh_lkjd/xfsudo
+    cd ~/abdhesh_lkjd/xfsudo
+    makepkg -si
+}
+# Tasks to be done
 
+setup_xst_st(){
+    cat << EOF
+            ############################################
+                Installing xst Terminal    
+            ############################################
+EOF
+
+    git clone https://aur.archlinux.org/xst-git.git ~/abdhesh_lkjd/xst-git
+    cd ~/abdhesh_lkjd/xst-git
+    makepkg -si
+
+    cat << EOF
+            ############################################
+                Installing st Terminal   
+            ############################################
+EOF
+
+    git clone https://aur.archlinux.org/st-git.git ~/abdhesh_lkjd/st-git
+    cd ~/abdhesh_lkjd/st-git
+    makepkg -si
+
+}
+
+setup_polybar(){
+    cat << EOF
+            ############################################
+                Installing Polybar    
+            ############################################
+EOF
+    git clone https://aur.archlinux.org/polybar.git ~/abdhesh_lkjd/polybar
+    cd ~/abdhesh_lkjd/polybar
+    makepkg -si
+}
+
+setup_powerlevezsh(){
+    cat << EOF
+            ############################################
+                Installing powerlevel10k    
+            ############################################
+EOF
+
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/powerlevel10k
+    echo 'source ~/.config/powerlevel10k/powerlevel10k.zsh-theme' >>~/.config/zsh/.zshrc
+
+}
 
 dir = $(pwd)
 
-$(mkdir ~/abdhesh_lkjd) &&
-
-
-
+show_intro
+install_packages
+create_work_dir
 setup_lf 
-setup_yay 
+setup_yay
+setup_xfsudo
+setup_xst_st
+setup_polybar
+setup_powerlevezsh
 
-cat << EOF
-############################################
-    Installing xfsudo password prompt    
-############################################
-EOF
-
-git clone https://aur.archlinux.org/xfsudo.git ~/abdhesh_lkjd/xfsudo
-cd ~/abdhesh_lkjd/xfsudo
-makepkg -si
-
-cat << EOF
-############################################
-    Installing xst Terminal    
-############################################
-EOF
-
-git clone https://aur.archlinux.org/xst-git.git ~/abdhesh_lkjd/xst-git
-cd ~/abdhesh_lkjd/xst-git
-makepkg -si
-
-cat << EOF
-############################################
-    Installing st Terminal   
-############################################
-EOF
-
-git clone https://aur.archlinux.org/st-git.git ~/abdhesh_lkjd/st-git
-cd ~/abdhesh_lkjd/st-git
-makepkg -si
-
-cat << EOF
-############################################
-    Installing Polybar    
-############################################
-EOF
-git clone https://aur.archlinux.org/polybar.git ~/abdhesh_lkjd/polybar
-cd ~/abdhesh_lkjd/polybar
-makepkg -si
 
 sudo rm -r ~/abdhesh_lkjd
 
 cat << EOF
-############################################
-    Installation done    
-############################################
+            ############################################
+                        Installation done    
+            ############################################
 EOF
 
 sudo rm -r $dir/.git
@@ -119,36 +135,31 @@ sudo rm -r $dir/.git
 cd $dir
 
 cat << EOF
-############################################
-    Installing Configs    
-############################################
+            ############################################
+                        Installing Configs              
+            ############################################
 EOF
 
 cp -r $dir/. ~/
 
 cat << EOF
-############################################
-    zsh Setup    
-############################################
+            ############################################
+                        zsh Setup    
+            ############################################
 EOF
 
 chsh -s $(which zsh)
-
-cat << EOF
-############################################
-    Installing powerlevel10k    
-############################################
-EOF
-
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/powerlevel10k
-echo 'source ~/.config/powerlevel10k/powerlevel10k.zsh-theme' >>~/.config/zsh/.zshrc
 
 cd ~/
 
 rm ~/install_config.sh
 
 cat << EOF
-############################################
-    Installation Done    
-############################################
+            ############################################
+                        Installation Done    
+            ############################################
 EOF
+
+cd
+sudo rm -r $dir
+cd

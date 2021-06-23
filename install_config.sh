21e56db464ac83ae98d1df3de5f8e39f2d385e85
abdheshnayak <abdheshnayak@gmail.com>
2021-06-23 11:09:22 +0545

    bug-fix

--- install_config.sh
+++ install_config.sh
@@ -18,7 +18,7 @@ EOF
 }
 
 install_packages(){
-    sudo pacman -Syu bspwm sxhkd feh i3-gaps maim xclip picom rofi base-devel git zsh dmenu ranger htop  cmake go  ttf-font-awesome ttf-jetbrains-mono ttf-indic-otf alsa-card-profiles alsa-lib alsa-plugins alsa-topology-conf alsa-ucm-conf alsa-utils pulseaudio-alsa zita-alsa-pcmi xorg-xset dunst unclutter mpc
+    sudo pacman -Syu bspwm sxhkd feh i3-gaps maim xclip picom rofi base-devel git zsh dmenu ranger htop  cmake go  ttf-font-awesome ttf-indic-otf alsa-card-profiles alsa-lib alsa-plugins alsa-topology-conf alsa-ucm-conf alsa-utils pulseaudio-alsa zita-alsa-pcmi xorg-xset dunst unclutter mpc
 }
 
 
@@ -43,7 +43,7 @@ EOF
 }
 
 install_aur_packages(){
-    yay -S lf xfsudo xst-git st-git polybar ttf-material-design-icons
+    yay -S lf xfsudo xst-git st-git polybar ttf-material-design-icons nerd-fonts-jetbrains-mono
 }
 
 

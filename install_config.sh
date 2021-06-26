492fa39a28b71144b3e5f7b999cc259bafa7a866
abdheshnayak <abdheshnayak@gmail.com>
2021-06-26 18:24:18 +0545

    bug fix

--- install_config.sh
+++ install_config.sh
@@ -146,5 +146,3 @@ ln -s ~/.profile ~/.zprofile
 
 cd ~/
 
-newperms "%wheel ALL=(ALL) ALL #LARBS
-%wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/packer -Syu,/usr/bin/packer -Syyu,/usr/bin/systemctl restart NetworkManager,/usr/bin/rc-service NetworkManager restart,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/yay,/usr/bin/pacman -Syyuw --noconfirm"

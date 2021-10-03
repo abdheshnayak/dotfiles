c51bbb705b3aa5d5e0ed971f69c23d83c7794aaf
Abdhesh Nayak <abdheshnayak@gmail.com>
2021-10-03 10:27:52 +0530

    minor changes

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,13 +5,26 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1$USER.log /tmp/polybar2$USER.log
-polybar black >>/tmp/polybar1$USER.log 2>&1 & disown
-#polybar top-panel >>/tmp/polybar2.log 2>&1 & disown
+# # Launch bar1 and bar2
+# echo "---" | tee -a /tmp/polybar1$USER.log /tmp/polybar2$USER.log
+# polybar black >>/tmp/polybar1$USER.log 2>&1 & disown
+# #polybar top-panel >>/tmp/polybar2.log 2>&1 & disown
 
-echo "---" | tee -a "/tmp/polybar1$USER.log" /tmp/polybar2$USER.log
-polybar black-top >>/tmp/polybar1$USER.log 2>&1 & disown
+# echo "---" | tee -a "/tmp/polybar1$USER.log" /tmp/polybar2$USER.log
+# polybar black-top >>/tmp/polybar1$USER.log 2>&1 & disown
 
 
-echo "Bars launched..."
+# echo "Bars launched..."
+
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload black &
+    MONITOR=$m polybar --reload black-top &
+  done
+else
+  polybar --reload black &
+  polybar --reload black-top &
+fi
+
+setmonitor.sh

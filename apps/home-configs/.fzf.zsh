d54bfef5201440f36b77b0f4201c1883fef13438
Abdhesh Nayak <abdheshnayak@gmail.com>
2022-03-02 19:49:42 +0530

    :tada: init

--- apps/home-configs/.fzf.zsh
+++ apps/home-configs/.fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */home/vision/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/vision/.fzf/bin"
+  export PATH="${PATH:+${PATH}:}~/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/vision/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "~/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/vision/.fzf/shell/key-bindings.zsh"
+source "~/.fzf/shell/key-bindings.zsh"

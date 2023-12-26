715a602295ceaa03403581c3ee70aeb7d994e965
Abdhesh Nayak <abdheshnayak@gmail.com>
2023-12-26 15:30:48 +0530

    :art: Changes for dap

--- apps/nvim/.config/nvim/lua/globals.lua
+++ apps/nvim/.config/nvim/lua/globals.lua
@@ -1,3 +1,9 @@
 vim.g.home = os.getenv("HOME")
 vim.g.root_dir = vim.fn.getcwd() -- this is tab local directory
 vim.g.nvim_dir = vim.fn.stdpath("config")
+
+vim.g.nxt = {
+  home = os.getenv("HOME"),
+  project_root_dir = vim.fn.getcwd(), -- this is tab local directory
+  nvim_dir = vim.fn.stdpath("config"),
+}
